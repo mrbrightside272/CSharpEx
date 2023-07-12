0fdf1864b90d490823653b546d2d01f7ea51242a
Fanis <f.shigapov@inbox.ru>
2023-07-12 22:49:02 +0600

    Example 5

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    System.Console.Write("привет, ");
+    System.Console.Write(username);
+}
